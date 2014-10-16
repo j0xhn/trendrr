62af7d11f6d341a55198a7da9cad09ccd794e60e
Jess Haddow <jess.haddow@gmail.com>
2014-10-16 01:47:51 -0600

    Adds ng-repeat and mixitup animation change

--- basic-app/js/Controllers/listCtrl.js
+++ basic-app/js/Controllers/listCtrl.js
@@ -1,3 +1,11 @@
-function listCtrl($scope) {
+angular.module('trendrr').
+controller('listCtrl', function($scope, listService) {
 	$scope.test = "Angular Is Working";	
-}
+	$scope.list = [];
+	listService.getList().then(function(data){
+		for(var key in data){
+			$scope.list.push(data[key]);
+		}
+		console.log($scope.list);
+	});
+});
